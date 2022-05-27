96580c2df88eda2333eaaadec7758612e3df843c
Carlos Talero <taleroc@yahoo.com>
2022-05-27 03:43:30 -0500

    2.0

--- proyecto_app/lib/pages/datos.dart
+++ proyecto_app/lib/pages/datos.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class DatosPage extends StatefulWidget {
@@ -11,7 +14,54 @@ class _DatosPageState extends State<DatosPage> {
     return Scaffold(
       appBar: AppBar(title: Text('Pagina datos')),
       body: Center(
-        child: Text('Esta es la pagina Datos'),
+        child: AddData(),
+      ),
+    );
+  }
+}
+
+class AddData1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FloatingActionButton(
+        backgroundColor: Colors.green,
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('data')
+              .doc(FirebaseAuth.instance.currentUser?.uid)
+              .set({'text': 'data added through app2', 'descripcion': 'weno'});
+        },
+      ),
+    );
+  }
+}
+
+class AddData extends StatelessWidget {
+  List<String> daticos = [];
+  String prueba = "";
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('data').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            children: snapshot.data!.docs.map((document) {
+              daticos.add(document['text']);
+              print(daticos);
+              return Container(
+                child: Center(child: Text(document['text'])),
+              );
+            }).toList(),
+          );
+        },
       ),
     );
   }
--- proyecto_app/lib/pages/principal.dart
+++ proyecto_app/lib/pages/principal.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:proyecto_app/models/principalItem.dart';
 
@@ -30,10 +31,23 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  late final Set<Marker> markeres = new Set();
   static const _posicionInicial =
       CameraPosition(target: LatLng(4.606422, -74.069321), zoom: 15.0);
 
+  late LatLng _posActual = LatLng(4.606422, -74.069321);
+
   late GoogleMapController _googleMapController;
+
+  Future<void> checkubi() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    permission = await Geolocator.requestPermission();
+    if (permission == await Geolocator.requestPermission()) {
+      checkubi();
+    }
+  }
+
   @override
   void dispose() {
     _googleMapController.dispose();
@@ -41,20 +55,43 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    Geolocator.getCurrentPosition().then((currLocation) {
+      setState(() {
+        _posActual = new LatLng(currLocation.latitude, currLocation.longitude);
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    markeres.add(Marker(
+      markerId: const MarkerId('id del marker'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+      position: const LatLng(4.606422, -74.069321),
+      infoWindow: InfoWindow(
+        title: 'Parqueadero don vergas',
+        snippet: 'H:8am-8pm/Vehiculos:Carros,Motos/Disponibilidad: si',
+      ),
+    ));
     return Scaffold(
       body: GoogleMap(
+        myLocationEnabled: true,
         myLocationButtonEnabled: true,
         zoomControlsEnabled: false,
-        zoomGesturesEnabled: true,
+        zoomGesturesEnabled: false,
         initialCameraPosition: _posicionInicial,
         onMapCreated: (controller) => _googleMapController = controller,
+        markers: markeres,
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
         foregroundColor: Colors.black,
-        onPressed: () => _googleMapController
-            .animateCamera(CameraUpdate.newCameraPosition(_posicionInicial)),
+        onPressed: () => _googleMapController.animateCamera(
+            CameraUpdate.newCameraPosition(
+                CameraPosition(target: _posActual, zoom: 15.0))),
         child: const Icon(Icons.center_focus_strong),
       ),
     );
